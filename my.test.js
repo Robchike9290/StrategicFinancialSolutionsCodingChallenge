2aac40f03c7c1ebc0406b32e13943012cd50356c
Robert Czajka <robchike9290@gmail.com>
2022-06-02 16:51:17 -0500

    Final commit before recording.  Took break because I needed to be at work from 9:30 AM - 4 PM CDT.

--- my.test.js
+++ my.test.js
@@ -1,3 +1,8 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+
+import App from './src/App.js';
+
 test('adds 1 + 2 to equal 3', () => {
   expect(1+2).toBe(3);
 });
